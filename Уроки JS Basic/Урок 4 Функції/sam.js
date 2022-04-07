15529172b94346e22f34cce5cd9de9492bac79dc
Taras Sereda <taras.sereda93@gmail.com>
2022-04-07 20:46:30 +0300

    add sushi

--- Уроки JS Basic/Урок 4 Функції/sam.js
+++ Уроки JS Basic/Урок 4 Функції/sam.js
@@ -1,4 +1,28 @@
-function nameNe() {
+/* function nameNe() {
   console.log("Taras");
 }
-nameNe();
+nameNe(); */
+
+/* function num(i) {
+  for (let i = 1; i < 101; i++) {
+    console.log(i);
+  }
+}
+num(); */
+
+// function name(num) {
+//   console.log(num * 3);
+
+// }
+// name(2)
+
+// function calc(num) {
+//   if (num % 2 === 0) {
+//     console.log("парне");
+//   } else {
+//     console.log("непарне");
+//   }
+// }
+// calc(3);
+
+
