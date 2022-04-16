dc4bf2ac7054a35ac1e02cf0016356cd011621e0
Taras Sereda <taras.sereda93@gmail.com>
2022-04-16 12:40:02 +0300

    add

---
+++ rozetka trainig project/js/main.js
@@ -0,0 +1,8 @@
+
+// document.getElementById('test').onclick = function() {
+//     document.getElementById('test').classList.add('main--main-bg');
+// }
+
+document.querySelector('.header-menu').onclick = function() {
+        document.querySelector('.header-menu').classList.add('main--main-bg');
+    }
