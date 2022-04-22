1d74a292c2382b011d08564a661e26fce413b422
Taras Sereda <taras.sereda93@gmail.com>
2022-04-22 18:10:45 +0300

    add

--- rozetka trainig project/js/main.js
+++ rozetka trainig project/js/main.js
@@ -1,11 +1,31 @@
 const buttons = document.querySelectorAll(".nav-toggle");
 
 for (const button of buttons) {
-  button.addEventListener('click', function(event) {
-    document.querySelector('.shadow').classList.toggle('shadow--open');
-    document.querySelector('.nav').classList.toggle('nav--open');
-  })
-};
+  button.addEventListener("click", function (event) {
+    document.querySelector(".shadow").classList.toggle("shadow--open");
+    document.querySelector(".nav").classList.toggle("nav--open");
+  });
+}
 
+const catalogs = document.querySelectorAll(".btn-catalog");
 
+for (const catalog of catalogs) {
+  catalog.addEventListener("click", function (event) {
+    document.querySelector(".product-catalog").classList.toggle("product-catalog--open");
+    document.querySelector(".shadow").classList.toggle("shadow--open");
+ 
+  });
+}
+
+let closeBtn = document.querySelector(".closed-catalog");
+
+closeBtn.addEventListener('click', () =>{
+/*   document.querySelector(".product-catalog--open").classList.toggle("product-catalog");
+  document.querySelector(".shadow--open").classList.toggle("shadow"); */
+
+  document.querySelector(".product-catalog--open").classList.remove("product-catalog");
+  document.querySelector(".shadow--open").classList.remove("shadow");
+
+  console.log('sdf');
+})
 
