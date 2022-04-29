10d18006f6531557940bf111d643356b65962e45
Taras Sereda <taras.sereda93@gmail.com>
2022-04-29 20:13:32 +0300

    add

--- rozetka trainig project/js/main.js
+++ rozetka trainig project/js/main.js
@@ -26,5 +26,111 @@ document.querySelector(".shadow").addEventListener('click', ()=>{
 
 
 
+let slideIndex = 1;
+showSlides(slideIndex);
+
+/* Функция увеличивает индекс на 1, показывает следующй слайд*/
+function plusSlide() {
+  showSlides((slideIndex += 1));
+}
+
+/* Функция уменьшяет индекс на 1, показывает предыдущий слайд*/
+function minusSlide() {
+  showSlides((slideIndex -= 1));
+}
+
+/* Устанавливает текущий слайд */
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+/* Основная функция слайдера */
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("item");
+
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  slides[slideIndex - 1].style.display = "block";
+}
+let timer = setInterval(function () {
+  slideIndex++;
+  showSlides(slideIndex);
+}, 3000);
 
 
+
+
+
+
+
+/* const titleBtnOn = document.querySelector('.btn__title__empty');
+const titleBtnOf = document.querySelector('.btn__title__full');
+
+titleBtnOn.addEventListener("click", () => {
+  titleBtnOn.classList.add("btn__title__empty__of");
+  titleBtnOf.classList.add("btn__title__full__one");
+  console.log("on");
+});
+
+titleBtnOf.addEventListener("click", () => {
+  titleBtnOn.classList.remove("btn__title__empty__of");
+  titleBtnOf.classList.remove("btn__title__full__one");
+  console.log("of");
+}); */
+
+
+
+var btns = document.querySelectorAll('.btn__title')
+
+const titleBtnOn = document.querySelector('.btn__title__empty');
+const titleBtnOf = document.querySelector('.btn__title__full');
+
+
+
+
+btns.forEach(function(btn) {
+
+  btn.addEventListener('click', function(event) {
+    console.log(event.target.classList);
+
+  })
+
+})
+
+
+
+
+titleBtnOn.addEventListener("click", () => {
+  titleBtnOn.classList.add("btn__title__empty__of");
+  titleBtnOf.classList.add("btn__title__full__one"); 
+
+  console.log("on");
+});
+
+titleBtnOf.addEventListener("click", () => {
+  titleBtnOn.classList.remove("btn__title__empty__of");
+  titleBtnOf.classList.remove("btn__title__full__one");
+  console.log("of");
+});
+
+
+
+
+/*   if (titleBtnOn) {
+    titleBtnOn.classList.add("btn__title__empty__of");
+    titleBtnOf.classList.add("btn__title__full__one");
+    console.log("on");
+    
+  } else if (titleBtnOf)
+  titleBtnOn.classList.remove("btn__title__empty__of");
+  titleBtnOf.classList.remove("btn__title__full__one");
+  console.log("of"); */
