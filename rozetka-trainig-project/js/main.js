ea03132ca5e9193b9b2247b7dac2a7bf8ef02f1c
Taras Sereda <taras.sereda93@gmail.com>
2022-05-06 00:01:48 +0300

    add

--- rozetka-trainig-project/js/main.js
+++ rozetka-trainig-project/js/main.js
@@ -29,6 +29,11 @@ document.querySelector(".shadow").addEventListener("click", () => {
   document.querySelector(".nav").classList.remove("nav--open");
 });
 
+
+
+
+
+
 let slideIndex = 1;
 showSlides(slideIndex);
 
@@ -91,7 +96,7 @@ const products = [
   {
     id: 0 ,
     name: 'Ноутбук HP Pavilion Gaming 15-ec2013ua',
-    img: 'rozetka trainig project/img/new-goods/tov-1.jpeg',
+    image: './img/new-goods/tov-1.jpeg',
     oldPrice: 28999 ,
     curentPrice: 25999 ,
     description: 'Ігровий ноутбук'
@@ -99,7 +104,7 @@ const products = [
   {
     id: 1 ,
     name: 'Ноутбук Acer Aspire 7 A715-42g-ROVS',
-    img: 'rozetka trainig project/img/new-goods/tov-2.jpeg',
+    image: './img/new-goods/tov-2.jpeg',
     oldPrice: 27999,
     curentPrice: 25999,
     description: 'Робочий ноутбук'
@@ -107,7 +112,7 @@ const products = [
   {
     id: 2 ,
     name: 'Ноутбук ASUS TUF Gaming F15 FX506LH-HN042 ',
-    img: 'rozetka trainig project/img/new-goods/tov-3.jpeg',
+    image: './img/new-goods/tov-3.jpeg',
     oldPrice: 35999,
     curentPrice: 33999,
     description: 'Ігровий ноутбук'
@@ -115,7 +120,7 @@ const products = [
   {
     id: 3 ,
     name: 'Ноутбук ASUS ExpertBook BA1500CDA-BQ0485 15.6" IPS',
-    img: 'rozetka trainig project/img/new-goods/tov-4.jpeg',
+    image: './img/new-goods/tov-4.jpeg',
     oldPrice: 17999,
     curentPrice: 17499,
     description: 'Робочий ноутбук'
@@ -123,7 +128,7 @@ const products = [
   {
     id: 4 ,
     name: 'Ноутбук ASUS TUF Gaming F15 FX506HM-HN232',
-    img: 'rozetka trainig project/img/new-goods/tov-5.jpeg',
+    image: './img/new-goods/tov-5.jpeg',
     oldPrice: 46999,
     curentPrice: 44999,
     description: 'Ігровий ноутбук'
@@ -131,7 +136,7 @@ const products = [
   {
     id: 5 ,
     name: 'Ноутбук Acer Aspire 7 A715-42G-R0VS (NH.QBFEU.00A)',
-    img: 'rozetka trainig project/img/new-goods/tov-6.jpeg',
+    image: './img/new-goods/tov-6.jpeg',
     oldPrice: 27999,
     curentPrice: 25999,
     description: 'Робочий ноутбук'
@@ -139,7 +144,7 @@ const products = [
   {
     id: 6 ,
     name: 'Ноутбук Pixus Rise Grey',
-    img: 'rozetka trainig project/img/new-goods/tov-7.jpeg',
+    image: './img/new-goods/tov-7.jpeg',
     oldPrice: 7999,
     curentPrice: 7499,
     description: 'Робочий ноутбук'
@@ -147,7 +152,7 @@ const products = [
   {
     id: 7 ,
     name: 'Ноутбук Acer Swift 1 SF114-34-P1A1 (NX.A77EU.00V)',
-    img: 'rozetka trainig project/img/new-goods/tov-8.jpeg',
+    image: './img/new-goods/tov-8.jpeg' ,
     oldPrice: 15999,
     curentPrice: 14999,
     description: 'Робочий ноутбук'
@@ -156,7 +161,77 @@ const products = [
 ];
 
 
+/* for ( let a in products){
+  const imgElem = products[a].Image;
+  const goodsname = products[a].name;
+  const priceNa = products[a].oldPrice;
+  const priceActual = products[a].curentPrice;
+
+  let picture = document.querySelector('.goods_img_picture').innerHTML = imgElem;
+  let goodsTitle = document.querySelector('.goods_title').innerHTML = goodsname;
+  let googsNa = document.querySelector('.googs_price_na').innerHTML = priceNa + ' грн.';
+  let price = document.querySelector('.goods_price_actual').innerHTML = priceActual + ' грн.';
+
+  console.log(a);
+} */
+
+
+
+const template = document.querySelector('.goods');
+const productTemplate = document.querySelector('.goods_inner');
+const productContainer = document.querySelector('.goods_card')
+
+template.remove();
+
+getData().forEach(item => {
+  const productElem = document.importNode(productTemplate, true);
+  productElem.querySelector('.goods_img_picture').src = item.image;
+  productElem.querySelector('.goods_title').textContent = item.name;
+  productElem.querySelector('.googs_price_na').textContent = item.oldPrice;
+  productElem.querySelector('.goods_price_actual').textContent = item.curentPrice;
+  productContainer.appendChild(productElem);
+
+});
+
+function getData() {
+  return products;
+}
 
 
 
 
+
+
+
+/* 
+const addBtn = document.querySelector(".add-btn");
+
+function goods(products) {
+  for (let i = 0; i < products.length; i++) {
+    let id = (identificator = products[i].id);
+    console.log(id);
+
+    if (id === 0) {
+      const imgElem = products[i].image;
+      const goodsname = products[i].name;
+      const priceNa = products[i].oldPrice;
+      const priceActual = products[i].curentPrice;
+
+
+      let picture = (document.querySelector(".goods_img_picture").src =
+        imgElem);
+      let goodsTitle = (document.querySelector(".goods_title").innerHTML =
+        goodsname);
+      let googsNa = (document.querySelector(".googs_price_na").innerHTML =
+        priceNa + " грн.");
+      let price = (document.querySelector(".goods_price_actual").innerHTML =
+        priceActual + " грн.");
+    }
+  }
+}
+
+goods(products); */
+
+/* addBtn.addEventListener('click', () => {
+  goods(products)
+}) */
---
+++ training-adaptive-css/box-adaptive/main.js
@@ -0,0 +1,25 @@
+const products = [
+  {
+    id: 0,
+    name: "Ноутбук HP Pavilion Gaming 15-ec2013ua",
+    img: "rozetka trainig project/img/new-goods/tov-1.jpeg",
+    oldPrice: 28999,
+    curentPrice: 25999,
+    description: "Ігровий ноутбук",
+  },
+  {
+    id: 1,
+    name: "Ноутбук Gaming 15-ec2013ua",
+    img: "rozetka trainig project/img/new-goods/tov-1.jpeg",
+    oldPrice: 22999,
+    curentPrice: 21999,
+    description: "Ігровий ноутбук",
+  }
+];
+
+console.log(products);
+document.write(products[1].name)
+document.write(products[1].img)
+document.write(products[1].oldPrice)
+document.write(products[1].curentPrice)
+document.write(products[1].description)
