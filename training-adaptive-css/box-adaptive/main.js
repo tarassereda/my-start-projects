6bc2dc4f6d8bbc462860e0bf615311913b6ddd7d
tarasereda <taras.sereda93@gmail.com>
2022-05-19 11:21:42 +0300

    add

--- training-adaptive-css/box-adaptive/main.js
+++ training-adaptive-css/box-adaptive/main.js
@@ -1,4 +1,4 @@
-const products = [
+/* const products = [
   {
     id: 0,
     name: "Ноутбук HP Pavilion Gaming 15-ec2013ua",
@@ -14,31 +14,54 @@ const products = [
     oldPrice: 22999,
     curentPrice: 21999,
     description: "Ігровий ноутбук",
-  }
+  },
 ];
 
-const template = document.querySelector('.goods_catalog');
-const containerGodds = document.querySelector('.goods_catalog_item');
-const goods = document.querySelector('.goods');
+const template = document.querySelector(".goods_catalog");
+const containerGodds = document.querySelector(".goods_catalog_item");
+const goods = document.querySelector(".goods");
 
-template.remove()
+template.remove();
 
-getData().forEach(item => {
+getData().forEach((item) => {
   const createElem = document.importNode(containerGodds, true);
-  createElem.querySelector('.picture').src = item.img;
-  createElem.querySelector('.goods_title').textContent = item.name;
-  createElem.querySelector('.old_price').textContent = item.oldPrice;
-  createElem.querySelector('.new_price').textContent = item.curentPrice;
-  console.log(createElem);
-  goods.appendChild(createElem)
+  createElem.querySelector(".picture").src = item.img;
+  createElem.querySelector(".goods_title").textContent = item.name;
+  createElem.querySelector(".old_price").textContent = item.oldPrice;
+  createElem.querySelector(".new_price").textContent = item.curentPrice;
 
-  
+  goods.appendChild(createElem);
 });
 
+function getData() {
+  return products;
+}
+ */
 
 
-function getData () {
-  return products
+const arr = ["hello", 4188, "Test", 99, "BMW"];
+
+function musOut() {
+  let p = document.getElementById("out");
+  let str = "";
+  for (let i = 1; i < arr.length; i++) {
+    str += i + " - " + arr[i] + "<br>";
+  }
+  p.innerHTML = str;
+}
+musOut();
+
+function btn1() {
+  let inp = document.getElementById("inp1").value;
+  arr.push(inp);
+  musOut();
+  document.getElementById("inp1").value = '';
+}
+
+function btn2() {
+  let inp = document.getElementById("inp1").value;
+  arr.pop(inp);
+  musOut();
 }
 
 
@@ -46,18 +69,58 @@ function getData () {
 
 
 
-const arr = ['hello', 4188, {name: 'Taras'}, 99, 'BMW'];
 
 
-function btn1 () {
-  document.getElementById('inp1').value;
-  for (let i = 0; i < arr.length; i++) {
-    let summ = '';
-  summ.push(arr[i])
-  console.log(arr[i]);
-    
+
+
+
+
+
+
+let leftSize = 0;
+/* autoSlider() */
+let timer;
+
+function autoSlider(params) {
+  timer = setTimeout(sliderLeft, 1000);
+}
+
+
+
+function sliderLeft() {
+  let poloska = document.querySelector(".polosa");
+
+  leftSize = leftSize - 300;
+
+  if (leftSize < -1200) {
+    leftSize = 0;
+/*     clearTimeout(timer) */
   }
 
-
+  poloska.style.left = leftSize + "px";
+  console.log(poloska);
+/*   autoSlider() */
 }
+
+/* let rghtSize = 0;
+
+function slideeRight() {
+  let poloskaRight = document.querySelector(".polosa");
+
+  rghtSize = rghtSize + 300;
+
+  if (rghtSize > 1200) {
+    rghtSize = 0;
+  }
+
+  poloskaRight.style.right = rghtSize + "px";
+  console.log(poloskaRight);
+} */
+
+
+
+
+
+
+
 
