702a84928bb1de941bffd42c8645bbb9c0b2b95e
Taras Sereda <taras.sereda93@gmail.com>
2022-09-12 20:49:18 +0300

    add

---
+++ learn_javascript/5_ cycle_in_javascript/main.js
@@ -0,0 +1,107 @@
+/* for (let i = 1; i <= 100; i++) {
+    console.log(i);
+} */
+/* for (let i = 11; i <= 33; i++) {
+    console.log(i);
+} */
+
+/* for (let i = 0; i <= 100; i+=2) {
+    console.log(i);
+} */
+
+/* for (let i = 1; i <= 99; i+=2) {
+    console.log(i);
+} */
+
+/* for (let i = 100; i > 0; i--) {
+    console.log(i);
+} */
+
+//Перебор массива циклом for-of в JavaScript
+
+/* let arr = ["a", "b", "c", "d", "e"];
+
+for (let item of arr) {
+  console.log(item);
+} */
+
+//Перебор объекта циклом for-in в JavaScript
+
+/* let obj = { x: 1, y: 2, z: 3 };
+for (let key in obj) {
+  console.log(key);
+  console.log(obj[key]);
+} */
+
+//Цикл while в JavaScript
+
+/* let item = 1;
+while (item <= 10) {
+  console.log(item);
+  item++;
+} */
+
+/* let item = 11;
+while(item <= 33){
+    console.log(item);
+    item++
+} */
+
+//Цикл for для массивов в JavaScript
+
+/* let arr = ['a', 'b', 'c', 'd', 'e'];
+for (let item = 0; item < arr.length; item++) {
+    console.log(arr[item]);
+} */
+
+
+/* let arr = ['a', 'b', 'c', 'd', 'e'];
+for (let item = 1; item < arr.length - 1; item++) {
+    console.log(arr[item]);
+} */
+
+/* let arr = ['a', 'b', 'c', 'd', 'e'];
+for (let item = arr.length - 1; item >= 0; item--) {
+    console.log(arr[item]);   
+} */
+
+/* let arr = ['a', 'b', 'c', 'd', 'e'];
+		
+for (let i = 0; i < arr.length; i++) {
+	console.log(arr[i]);
+} */
+
+
+
+//Условия в циклах в JavaScript
+
+/* let arr = [2, 5, 9, 15, 1, 4];
+for (let elem of arr) {
+  if (arr[elem] >= 3 && arr[elem] <= 10) {
+    console.log(arr[elem]);
+  }
+} */
+
+/* let obj = { a: 1, b: 2, c: 3, d: 4, e: 5 };
+for (let key in obj) {
+  if (obj[key] % 2 == 1) {
+    console.log(obj[key]);
+  }
+} */
+
+//Накопление чисел в циклах JavaScript
+
+/* let res = 0;
+
+for (let elem = 1; elem <= 99 ; elem++) {
+    res += elem
+    if (res % 2 == 1) {
+        console.log(res);
+    }
+
+}   */ 
+
+//Накопление элементов массивов в циклах JavaScript
+
+
+
